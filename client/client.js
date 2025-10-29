eff8ae0fa7fa2cb7f32c72aa33ec502446f9e29c
John Castleman <john@castleman.dev>
2025-10-29 12:10:47 -0500

    Add file output capability with stream support

--- client/client.js
+++ client/client.js
@@ -1,3 +1,4 @@
+import { createWriteStream } from 'fs';
 import { Settings } from './settings.js';
 import { rankingsMetadataToString, playerToString, playerToTabDelimitedString } from './utils.js';
 import { fetchGeeksquadronRankings } from '../server/index.js';
@@ -13,44 +14,73 @@ async function getRankings(rankingType, position) {
 
 function displayRankings(rankings, options = {}) {
   const { players, metadata } = rankings;
-  const { displayMaxPlayers = Settings.displayMaxPlayers ?? null, verbose = Settings.verbose ?? false } = options;
+  const { 
+    displayMaxPlayers = Settings.displayMaxPlayers ?? null, 
+    verbose = Settings.verbose ?? false,
+    outputFile = Settings.outputFile ?? null 
+  } = options;
 
-  const title = rankingsMetadataToString(metadata, verbose);  
-  process.stdout.write(`\n${title}\n`);
-  process.stdout.write('=' + '='.repeat(title.length) + '\n\n');
+  const outStream = outputFile ? createWriteStream(outputFile) : process.stdout;
 
-  if (displayMaxPlayers != null && displayMaxPlayers !== 0) {
-    players.slice(0, displayMaxPlayers).forEach(player => {
-      process.stdout.write(playerToString(player) + '\n');
-    });
-    console.log('... (showing', displayMaxPlayers, 'of', players.length, 'players)');
-  } else {
-    players.forEach(player => {
-      process.stdout.write(playerToString(player) + '\n');
-    });
+  try {
+    const title = rankingsMetadataToString(metadata, verbose);  
+    outStream.write(`\n${title}\n`);
+    outStream.write('=' + '='.repeat(title.length) + '\n\n');
+
+    if (displayMaxPlayers != null && displayMaxPlayers !== 0) {
+      players.slice(0, displayMaxPlayers).forEach(player => {
+        outStream.write(playerToString(player) + '\n');
+      });
+      console.log('... (showing', displayMaxPlayers, 'of', players.length, 'players)');
+    } else {
+      players.forEach(player => {
+        outStream.write(playerToString(player) + '\n');
+      });
+    }
+    
+    outStream.write('\n');
+
+    if (outputFile) {
+      console.info(`Output written to: ${outputFile}`);
+    }
+  } finally {
+    if (outputFile) {
+      outStream.end();
+    }
   }
-  
-  process.stdout.write('\n');
 }
 
-function dumpRankingsToTabDelimited(rankings) {
+function dumpRankingsToTabDelimited(rankings, options = {}) {
   const { players, metadata } = rankings;
+  const { outputFile = Settings.outputFile ?? null } = options;
 
-  const title = rankingsMetadataToString(metadata) + " (tab-delimited)";  
-  console.log(`\n${title}`);
-  console.log('=' + '='.repeat(title.length) + '\n');
+  const outStream = outputFile ? createWriteStream(outputFile) : process.stdout;
 
-  const header = Settings.tabDelimitedHeader[metadata.rankingType];
-  
-  if (header) {
-    process.stdout.write(header + '\n');
+  try {
+    const title = rankingsMetadataToString(metadata) + " (tab-delimited)";  
+    console.log(`\n${title}`);
+    console.log('=' + '='.repeat(title.length) + '\n');
+
+    const header = Settings.tabDelimitedHeader[metadata.rankingType];
+    
+    if (header) {
+      outStream.write(header + '\n');
+    }
+
+    players.forEach(player => {
+      outStream.write(playerToTabDelimitedString(player) + '\n');
+    });
+    
+    outStream.write('\n');
+
+    if (outputFile) {
+      console.info(`Output written to: ${outputFile}`);
+    }
+  } finally {
+    if (outputFile) {
+      outStream.end();
+    }
   }
-
-  players.forEach(player => {
-    process.stdout.write(playerToTabDelimitedString(player) + '\n');
-  });
-  
-  process.stdout.write('\n');
 }
 
 export {
